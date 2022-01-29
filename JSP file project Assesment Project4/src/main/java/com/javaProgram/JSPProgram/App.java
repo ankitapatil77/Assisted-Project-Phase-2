f3bd62ba0d1f331bcd64b4778d504417636f271b
Ankita Patil <ankitapatil7799@gmail.com>
2022-01-29 19:50:33 +0530

    Jsp file project

---
+++ JSP file project Assesment Project4/src/main/java/com/javaProgram/JSPProgram/App.java
@@ -0,0 +1,53 @@
+package com.javaProgram.JSPProgram;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class App
+ */
+@WebServlet("/App")
+public class App extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public App() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String pId = request.getParameter("productId");
+		String pName = request.getParameter("productName");
+		String pPrice = request.getParameter("productPrice");
+		
+		HttpSession theSession = request.getSession();
+		
+		theSession.setAttribute("pid", pId);
+		theSession.setAttribute("pname", pName);
+		theSession.setAttribute("pprice", pPrice);
+		
+		response.sendRedirect("Display.jsp");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
